633b807ad08fb1b344b5db7b08d9bae115e0e496
Hans Engel <engel@engel.uk.to>
2008-07-28 12:29:06 -0700

    Regex hackery.. URL click-ifying updated, and lots of hacks to make things work

--- chrome/content/twitkit.js
+++ chrome/content/twitkit.js
@@ -249,7 +249,14 @@ var Tweetbar = {
 	 */
 	expand_status:
 		function (s) {
-			return s.toString().replace(/\</,'&lt;').replace(/((?:ftp|https?):\/\/\S+)/g, this.anchor_tag('$1')).replace(/\@([0-9a-z_A-Z]+)/g, this.anchor_tag(Tweetbar.protocol + ':\/\/twitter.com/$1'.toLowerCase(),'@$1','$1 ' + this._('misc.onTwitter')));
+			ret = s.toString();
+			ret = ret.replace(/\</,'&lt;');
+			var re = new RegExp('(<\\w+.*?>|[^=!:\'"/]|^)((?:https?:\/\/)|(?:www\.))([-\\w]+(?:\.[-\\w]+)*(?::\\d+)?(?:/(?:(?:[~\\w\\+%-]|(?:[,.;@:][^\\s$]))+)?)*(?:\\?[\\w\\+%&=.;:-]+)?(?:\#[\\w\-\.]*)?)([\\!\\)\\.\\s]|<|$)', 'g');
+			ret = ret.replace(re, '$1' + this.anchor_tag('$2$3') + '$4');
+			ret = ret.replace(/<a href="www/g, '<a href="http:\/\/www');
+			ret = ret.replace(/<a href="([^\s]+) (.+)" target(.+)>([^\s]+) (.+)<\/a>/, '<a href="$1" target$3>$1</a> $5');
+			ret = ret.replace(/\@([0-9a-z_A-Z]+)/g, this.anchor_tag(Tweetbar.protocol + ':\/\/twitter.com/$1'.toLowerCase(),'@$1','$1 ' + this._('misc.onTwitter')));
+			return ret;
 		},
 	/**
 	 * create_status_object ( obj )
