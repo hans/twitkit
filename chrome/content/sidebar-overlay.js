8b5ffdf2c89d76e3b3ec9177d01c880da004039a
Hans Engel <engel@engel.uk.to>
2008-08-10 07:48:23 -0700

    Add JSDoc to sidebar-overlay.js

--- chrome/content/sidebar-overlay.js
+++ chrome/content/sidebar-overlay.js
@@ -1,7 +1,27 @@
+/**
+ * A class to assist the browser overlay dialog
+ * 
+ * @class
+ */
 var TwitOverlay = {
+	/**
+	 * The service responsible for retrieving and setting preferences
+	 */
 	mPrefRoot: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch2),
+	/**
+	 * Service used to manipulate XUL dialogs
+	 */
 	mWindowMediator: Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator),
 	
+	/**
+	 * Retrieve a preference
+	 * 
+	 * @param {String} aName The name of the preference.
+	 * @param {String} aDefault A default value to return if something goes wrong.
+	 * @returns {Mixed} The preference's value or aDefault on error.
+	 * @methodOf TwitOverlay
+	 * @since 1.0
+	 */
 	getPref: function(aName, aDefault)
 	{
 		try
@@ -21,7 +41,12 @@ var TwitOverlay = {
 		
 		return aDefault;
 	},
-	
+	/**
+	 * Open TwitKit's Preferences window
+	 * 
+	 * @methodOf TwitOverlay
+	 * @since 1.0
+	 */
 	openPrefs: function()
 	{
 		var dialog = this.mWindowMediator.getMostRecentWindow("TwitKit:Preferences");
@@ -33,7 +58,12 @@ var TwitOverlay = {
 		
 		openDialog("chrome://twitkit/content/preferences.xul", "_blank", "chrome,titlebar,centerscreen," + ((this.getPref("browser.preferences.instantApply", false))?"dialog=no":"modal"));
 	},
-	
+	/**
+	 * Open TwitKit's About window
+	 * 
+	 * @methodOf TwitOverlay
+	 * @since 1.0
+	 */
 	openAbout: function()
 	{
 		var dialog = this.mWindowMediator.getMostRecentWindow("TwitKit:About");
