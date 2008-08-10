d7cbe1eb34b82b48c3045d8cd2c5b7ef4f11a210
Hans Engel <engel@engel.uk.to>
2008-08-10 07:58:06 -0700

    Clean up sidebar-overlay.js

--- chrome/content/sidebar-overlay.js
+++ chrome/content/sidebar-overlay.js
@@ -7,11 +7,11 @@ var TwitOverlay = {
 	/**
 	 * The service responsible for retrieving and setting preferences
 	 */
-	mPrefRoot: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch2),
+	prefService: Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefBranch2),
 	/**
 	 * Service used to manipulate XUL dialogs
 	 */
-	mWindowMediator: Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator),
+	windowService: Components.classes["@mozilla.org/appshell/window-mediator;1"].getService(Components.interfaces.nsIWindowMediator),
 	
 	/**
 	 * Retrieve a preference
@@ -22,57 +22,59 @@ var TwitOverlay = {
 	 * @methodOf TwitOverlay
 	 * @since 1.0
 	 */
-	getPref: function(aName, aDefault)
-	{
-		try
-		{
-			var pb = this.mPrefRoot;
-			switch (pb.getPrefType(aName))
-			{
-				case pb.PREF_STRING:
-					return pb.getComplexValue(aName,this.mComponents.interfaces.nsISupportsString).data
-				case pb.PREF_BOOL:
-					return pb.getBoolPref(aName);
-				case pb.PREF_INT:
-					return pb.getIntPref(aName);
-			}
-		}
-		catch (ex) { }
-		
-		return aDefault;
-	},
+	getPref:
+		function (aName, aDefault) {
+			try {
+				var pb = this.prefService;
+				switch ( pb.getPrefType(aName) ) {
+					case pb.PREF_STRING:
+						return pb.getCharPref(aName);
+					case pb.PREF_BOOL:
+						return pb.getBoolPref(aName);
+					case pb.PREF_INT:
+						return pb.getIntPref(aName);
+				}
+			} catch ( e ) { }
+			return aDefault;
+		},
 	/**
 	 * Open TwitKit's Preferences window
 	 * 
 	 * @methodOf TwitOverlay
 	 * @since 1.0
 	 */
-	openPrefs: function()
-	{
-		var dialog = this.mWindowMediator.getMostRecentWindow("TwitKit:Preferences");
-		if (dialog)
-		{
-			dialog.focus();
-			return;
-		}
-		
-		openDialog("chrome://twitkit/content/preferences.xul", "_blank", "chrome,titlebar,centerscreen," + ((this.getPref("browser.preferences.instantApply", false))?"dialog=no":"modal"));
-	},
+	openPrefs:
+		function () {
+			var dialog = this.windowService.getMostRecentWindow("TwitKit:Preferences");
+			if (dialog) {
+				dialog.focus();
+				return;
+			}
+			openDialog(
+				"chrome://twitkit/content/preferences.xul",
+				"_blank",
+				"chrome,titlebar,centerscreen," +
+				( ( this.getPref("browser.preferences.instantApply", false ) ) ? "dialog=no" : "modal" )
+			);
+		},
 	/**
 	 * Open TwitKit's About window
 	 * 
 	 * @methodOf TwitOverlay
 	 * @since 1.0
 	 */
-	openAbout: function()
-	{
-		var dialog = this.mWindowMediator.getMostRecentWindow("TwitKit:About");
-		if ( dialog )
-		{
-			dialog.focus();
-			return;
+	openAbout:
+		function () {
+			var dialog = this.windowService.getMostRecentWindow("TwitKit:About");
+			if ( dialog ) {
+				dialog.focus();
+				return;
+			}
+			openDialog(
+				"chrome://twitkit/content/about.xul",
+				"_blank",
+				"chrome,titlebar,centerscreen," +
+				( ( this.getPref("browser.preferences.instantApply", false ) ) ? "dialog=no" : "modal" )
+			);
 		}
-		
-		openDialog("chrome://twitkit/content/about.xul", "_blank", "chrome,titlebar,centerscreen," + ((this.getPref("browser.preferences.instantApply", false)) ? "dialog=no" : "modal" ));
-	}
 };
