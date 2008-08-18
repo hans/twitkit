22ed48674a464155819dc4c8004d1a056ecd4f5c
Hans Engel <engel@engel.uk.to>
2008-08-17 17:23:29 -0700

    Change some default preferences

--- defaults/preferences/twitkit.js
+++ defaults/preferences/twitkit.js
@@ -1,6 +1,6 @@
-pref('extensions.twitkit.showAppSource', false);
+pref('extensions.twitkit.showAppSource', true);
 pref('extensions.twitkit.refreshInterval', 30000);
-pref('extensions.twitkit.showNamesAs', 'screennames');
+pref('extensions.twitkit.showNamesAs', 'fullnames');
 pref('extensions.twitkit.colorScheme', 'deepBrown');
 pref('extensions.twitkit.fontSize', '100.01');
 pref('extensions.twitkit.active_panel', '');
