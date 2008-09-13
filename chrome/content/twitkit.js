9ffb48b3d1e3b8c4218f42c8ab6a72d5c6d79c98
Hans Engel <engel@engel.uk.to>
2008-09-13 12:03:42 -0700

    When tweets are truncated, provide link to full tweet [blueprint add-truncated-links implementation:implemented]

--- chrome/content/twitkit.js
+++ chrome/content/twitkit.js
@@ -327,7 +327,8 @@ var Tweetbar = {
 				'text': Tweetbar.expand_status(obj.text),
 				'created_at': Date.parse(obj.created_at || Date()),
 				'source': obj.source,
-				'favorited': obj.favorited
+				'favorited': obj.favorited,
+				'truncated': obj.truncated
 			}
 		},
 	/**
@@ -509,6 +510,9 @@ var Tweetbar = {
 					favorite = '<a class="re" href="javascript: Tweetbar.unfav_tweet(\'' + tweet.id + '\'); void 0;"><img id="fav-' + tweet.id + '" class="re" src="chrome://twitkit/skin/images/fav_remove.png" alt="" /></a>';
 				else
 					favorite = '<a class="re" href="javascript: Tweetbar.fav_tweet(\'' + tweet.id + '\'); void 0;"><img id="fav-' + tweet.id + '" class="re" src="chrome://twitkit/skin/images/fav_add.png" alt="" /></a>';
+				
+				if ( tweet.truncated )
+					tweet.text = tweet.text.replace(/\.\.\.$/, Tweetbar.anchor_tag(Tweetbar.protocol + '://twitter.com/' + tweet.user.screen_name + '/statuses/' + tweet.id, '...'));
 
 				return '<p class="pic"><a href="#" onclick="setReply(\'' + tweet.user.screen_name + '\');">'+ user_image + '</a>' +
 					   '<span class="re"><a class="re" href="#" onclick="setReply(\''+ tweet.user.screen_name + '\'); return false;"><img class="re" src="chrome://twitkit/skin/images/reply.png" alt="" /></a>&nbsp;' +
